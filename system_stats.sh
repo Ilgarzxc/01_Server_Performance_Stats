bd9a36d6c5201e6da40b1a98b98b3dc9497ff8e0
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-12 13:27:29 +0200

    refactored code and split into functions

---
+++ system_stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trap 'echo ""This script execution stopped due to an error in $LINENO"' ERR
+
+source ./modules/cpu_usage.sh
+source ./modules/ram_usage.sh
+
+printf "echo ===Choose required server information=== \n1) Overview \n2) CPU Usage \n3) RAM Usage \n4) Disk Usage \n5) Top 5 processes by CPU usage \n6) Top 5 processes by memory usage" 
+read -p "Please, insert required option:" choice
+
+case $choice in:
+    1) 
+    2) cpuUsage ;;
+    3) ramUsage ;;
+    4)
+    5)
+    6)
+esac

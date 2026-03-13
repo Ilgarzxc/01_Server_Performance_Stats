d4504f2a7e8b9d8af2546b45820601c23fccb37f
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-13 17:53:55 +0200

    markdown changes-6

--- modules/top5_cpu_usage.sh
+++ modules/top5_cpu_usage.sh
@@ -2,7 +2,7 @@
 function top5byCpu() {
     echo "TOP 5 PROCESS ORDER BY CPU USAGE:"
     echo "|---------------------------------------------------------------|"
-    printf "|%-38s | %-6s | %-10s | \n" "Process" "CPUUsg" "PID"
+    printf "| %-38s | %-6s | %-10s | \n" "Process" "CPUUsg" "PID"
     echo "|---------------------------------------------------------------|"
     ps aux --sort=-%cpu --no-headers | head -n 5 | \
     awk '{printf("| %-38s | %-6s | %-10s | \n", $11, $3, $2)}'
--- modules/top5_mem_usage.sh
+++ modules/top5_mem_usage.sh
@@ -2,7 +2,7 @@
 function top5byMem() {
     echo "TOP 5 PROCESS ORDER BY RAM USAGE:"
     echo "|---------------------------------------------------------------|"
-    printf "|%-38s | %-6s | %-10s | \n" "Process" "RAMUsg" "PID"
+    printf "| %-38s | %-6s | %-10s | \n" "Process" "RAMUsg" "PID"
     echo "|---------------------------------------------------------------|"
     ps aux --sort=-%mem --no-headers | head -n 5 | \
     awk '{printf("| %-38s | %-6s | %-10s | \n", $11, $4, $2)}'

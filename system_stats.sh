21ffb14bd657a274630df815099ed94a6d30e8f5
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-13 18:27:27 +0200

    refined required_cmds list

--- system_stats.sh
+++ system_stats.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 trap 'echo "This script execution stopped due to an error in $LINENO"' ERR
 
-REQUIRED_CMDS=("top" "free" "df" "uname" "awk" "grep" "uname" "lsb_release" "printf" "read" "bash")
+REQUIRED_CMDS=("top" "free" "df" "awk" "grep" "lsb_release")
 for cmd in "${REQUIRED_CMDS[@]}"; do
     if ! command -v "$cmd" &> /dev/null; then
         echo "Error: $cmd is not installed. Please install it and try again."

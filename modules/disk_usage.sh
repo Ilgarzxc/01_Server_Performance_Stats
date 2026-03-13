01d70fd2c8f96646917b9b95baf71721e7c9350f
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-13 17:52:41 +0200

    markdown changes-5

--- modules/disk_usage.sh
+++ modules/disk_usage.sh
@@ -6,12 +6,12 @@ function diskUsage() {
     read total_space used_space free_space percentage_used <<< $(echo $disk_usage | awk '{print $2,$3,$4,$5}')
 
     echo "DISK USAGE:"
-    echo "|--------------------------------------------------------------|"
-    printf "| %-35s | %-23s | \n" "Total space" $total_space
-    printf "| %-35s | %-23s | \n" "Used space" $used_space
-    printf "| %-35s | %-23s | \n" "Free space" $free_space
-    printf "| %-35s | %-23s | \n" "Percentage of used space" $percentage_used
-    echo "|--------------------------------------------------------------|"
+    echo "|---------------------------------------------------------------|"
+    printf "| %-35s | %-23s | \n" "Total space" "$total_space"
+    printf "| %-35s | %-23s | \n" "Used space" "$used_space"
+    printf "| %-35s | %-23s | \n" "Free space" "$free_space"
+    printf "| %-35s | %-23s | \n" "Percentage of used space" "$percentage_used"
+    echo "|---------------------------------------------------------------|"
 }
 
 
